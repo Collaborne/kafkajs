4b57b163c658faf2e9661424c10d129481ed80c5
Andreas Kohn <andreas.kohn@gmail.com>
2020-12-03 10:11:34 +0100

    Throw errors when metadata is missing
    Missing metadata means that something didn't correctly refresh it at a time it should
    have, and not having the metadata will likely mean something else (such as the
    `initializeConsumerOffsets` function) will crash.
    
    We should rather crash out at the point where it is clear what is the problem, instead
    of at a point where the only thing we know is "state is unexpected".

--- src/cluster/index.js
+++ src/cluster/index.js
@@ -218,6 +218,11 @@ module.exports = class Cluster {
     }
 
     const topicMetadata = metadata.topicMetadata.find(t => t.topic === topic)
+    if (!topicMetadata) {
+      throw new KafkaJSTopicMetadataNotLoaded('Topic metadata does not include required topic', {
+        topic,
+      })
+    }
     return topicMetadata ? topicMetadata.partitionMetadata : []
   }
 
@@ -238,7 +243,11 @@ module.exports = class Cluster {
       const metadata = partitionMetadata.find(p => p.partitionId === partitionId)
 
       if (!metadata) {
-        return result
+        throw new KafkaJSError('Unavailable partition metadata', {
+          topic,
+          partitionId,
+          partitionMetadata,
+        })
       }
 
       if (metadata.leader === null || metadata.leader === undefined) {
